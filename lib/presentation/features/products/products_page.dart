6061762df8f1734faa41331809976486445f861d
Renan <r.boni1994@gmail.com>
2024-09-06 21:00:02 +0200

    added products page

--- lib/presentation/features/products/products_page.dart
+++ lib/presentation/features/products/products_page.dart
@@ -1,11 +1,37 @@
 
 import 'package:flutter/material.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
 
 class ProductsPage extends StatelessWidget {
   const ProductsPage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return ShadCard(
+      padding: const EdgeInsets.all(0),
+      title: Padding(
+        padding: const EdgeInsets.symmetric(
+          vertical: 8,
+          horizontal: 16,
+        ),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            Text(
+              "Products",
+              style: ShadTheme.of(context).textTheme.table,
+            ),
+          ],
+        ),
+      ),
+      child: Expanded(
+        child: Column(
+          children: [
+            const Divider(height: 1),
+
+          ],
+        ),
+      ),
+    );
   }
 }
