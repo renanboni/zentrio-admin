9b7e087d1790e23b651545e5d17bd4be0db64a0c
Renan <r.boni1994@gmail.com>
2024-10-15 19:47:14 +0200

    create product type wip

--- lib/presentation/features/productTypes/create/create_product_type_page.dart
+++ lib/presentation/features/productTypes/create/create_product_type_page.dart
@@ -1,10 +1,50 @@
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+import 'package:responsive_framework/responsive_framework.dart';
+import 'package:shadcn_ui/shadcn_ui.dart';
+import 'package:zentrio_admin/presentation/components/dialog_footer.dart';
+import 'package:zentrio_admin/presentation/components/dialog_header.dart';
+import 'package:zentrio_admin/utils/extensions/context_ext.dart';
 
 class CreateProductTypePage extends StatelessWidget {
   const CreateProductTypePage({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    final theme = ShadTheme.of(context);
+
+    return Column(
+      children: [
+        DialogHeader(onTap: () => GoRouter.of(context).pop()),
+        Expanded(
+          child: MaxWidthBox(
+            maxWidth: context.maxWidth,
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const SizedBox(height: 24),
+                Text(
+                  "Create Product Type",
+                  style: theme.textTheme.h4,
+                ),
+                const SizedBox(height: 2),
+                Text(
+                  "Create a new product type to categorize your products.",
+                  style: theme.textTheme.muted,
+                ),
+                const SizedBox(height: 36),
+                ShadInputFormField(
+                  label: const Text("Value"),
+                )
+              ],
+            ),
+          ),
+        ),
+        DialogFooter(
+          onCancel: () => GoRouter.of(context).pop(),
+          onCreate: () {},
+        )
+      ],
+    );
   }
 }
